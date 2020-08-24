eee46ed1ad7f2fd77924b4b8de4c292b88ff9e1a
canbus <canbus@sohu.com>
2020-08-24 17:17:29 +0800

    定时器综合应用实验
    #功能说明
    模拟了一个自动关灯的场景，有个vKeyHitTask负责检测是否开灯，如果开灯会启动一个定时器，5s后自动关闭背景灯（Backlight）
    
    1.创建一个模拟按键的任务
    
    2.按键任务负责检测灯开关，但当灯打开后，启动定时器，5s后触发定时器回调函数
    
    3.定时器触发关闭背景灯

--- User/main.c
+++ User/main.c
@@ -5,86 +5,55 @@
 #include "los_queue.h"
 #include "los_swtmr.h"
 
-uint32_t taskSendID,taskRevID;
-uint32_t queueID;
+enum LIGHT_TYPE{
+	LIGHT_ON = 0,
+	LIGHT_OFF 
+}LIGHT_TYPE ;
 
-struct DATA{
-	uint32_t id;
-	char *msg;
-};
-
-struct DATA data1={100,"task1 data"};
-
-uint16_t timer1ID,timer2ID;
-void time1Handle(uint32_t arg)
+uint32_t vKeyHitTaskID;
+enum LIGHT_TYPE backLightState = LIGHT_OFF;
+uint16_t timerID;
+void timeHandle(uint32_t arg)
 {
-	printf("time over %d\n",arg);
+	backLightState = LIGHT_OFF;
+	printf("time over:light off\n");
 }
 
-void taskSend(uint32_t arg)
+void vKeyHitTask(uint32_t arg)
 {
-	uint32_t i,pri,retVal;
-	//一次性定时器
-	retVal = LOS_SwtmrCreate(100, LOS_SWTMR_MODE_ONCE, time1Handle, &timer1ID, 10 ,OS_SWTMR_ROUSES_IGNORE, OS_SWTMR_ALIGN_INSENSITIVE);
-	if(retVal != LOS_OK)
-	{
-		printf("create timer fail\n");
-	}
-	LOS_SwtmrStart(timer1ID);
-	//重复定时器
-	retVal = LOS_SwtmrCreate(500, LOS_SWTMR_MODE_PERIOD, time1Handle, &timer2ID, 20 ,OS_SWTMR_ROUSES_IGNORE, OS_SWTMR_ALIGN_INSENSITIVE);
-	if(retVal != LOS_OK)
-	{
-		printf("create timer fail\n");
-	}
-	LOS_SwtmrStart(timer2ID);
+	uint32_t retVal;
 	while(1)
 	{
-		LOS_TaskDelay(200);
-//		if(arg == data1.id)
-//			LOS_QueueWriteCopy(queueID, &data1, sizeof(struct DATA), 0);
-//		printf("write queue %d\n", arg);
+		LOS_TaskDelay(500);
+		if(backLightState == LIGHT_OFF)
+		{
+			backLightState = LIGHT_ON;
+			printf("time over:light on\n");
+			//重复定时器
+			retVal = LOS_SwtmrCreate(5000, LOS_SWTMR_MODE_ONCE, timeHandle, &timerID, 20 ,
+															OS_SWTMR_ROUSES_IGNORE, OS_SWTMR_ALIGN_INSENSITIVE);
+			if(retVal != LOS_OK)
+			{
+				printf("create timer fail\n");
+			}
+			LOS_SwtmrStart(timerID);
+		}
+
 	}
 }
 
-void taskRev(void)
-{
-	uint32_t size,retVal;
-	struct DATA queuedata;
-	while(1)
-	{
-//		retVal = LOS_QueueReadCopy(queueID, &queuedata, &size, 0);
-//		if(retVal == LOS_OK){
-//			printf("receive:%d,%s\n", queuedata.id, queuedata.msg);
-//		}
-		LOS_TaskDelay(10);
-	}
-}
 
 uint32_t taskCreate(void)
 {
 	uint32_t retVal;
-	TSK_INIT_PARAM_S taskInitParam;
+	TSK_INIT_PARAM_S taskInitParam={0};
 	
-	retVal = LOS_QueueCreate("queue", 5, &queueID, 0,sizeof(struct DATA));
-	
-	taskInitParam.pcName ="task1";
-	taskInitParam.pfnTaskEntry = (TSK_ENTRY_FUNC)&taskSend;
+	taskInitParam.pcName ="vKeyHitTask";
+	taskInitParam.pfnTaskEntry = (TSK_ENTRY_FUNC)&vKeyHitTask;
 	taskInitParam.usTaskPrio = 4;
 	taskInitParam.uwStackSize = 512;
 	taskInitParam.uwArg = 100;
-	retVal = LOS_TaskCreate(&taskSendID,&taskInitParam);
-	if(retVal != LOS_OK)
-	{
-		printf("LOS_TaskCreate() fail,%X\n", retVal);
-	}
-	
-	
-	taskInitParam.pcName ="taskRev";
-	taskInitParam.pfnTaskEntry = (TSK_ENTRY_FUNC)&taskRev;
-	taskInitParam.usTaskPrio = 3;
-	taskInitParam.uwStackSize = 512;
-	retVal = LOS_TaskCreate(&taskRevID,&taskInitParam);
+	retVal = LOS_TaskCreate(&vKeyHitTaskID,&taskInitParam);
 	if(retVal != LOS_OK)
 	{
 		printf("LOS_TaskCreate() fail,%X\n", retVal);
