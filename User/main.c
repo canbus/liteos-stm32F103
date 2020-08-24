79fb2d8267506037575fa68ada58a416c3656ed6
canbus <canbus@sohu.com>
2020-08-24 11:46:29 +0800

    1.创建最简单的二个任务

--- User/main.c
+++ User/main.c
@@ -1,85 +1,66 @@
-#include "stm32f10x.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_rcc.h"
+
 #include "los_sys.h"
 #include "los_task.ph"
 #include "los_memory.ph"
 
-#define LED1_ON() 	{GPIO_ResetBits(GPIOB, GPIO_Pin_10);}
-#define LED1_OFF() 	{GPIO_SetBits(GPIOB, GPIO_Pin_10);}
-#define LED2_ON() 	{GPIO_ResetBits(GPIOB, GPIO_Pin_11);}
-#define LED2_OFF() 	{GPIO_SetBits(GPIOB, GPIO_Pin_11);}
-
-static void Led1Task(void)
+void task1(void)
 {
-    while(1)
-    {
-        LED1_ON();
-        LOS_TaskDelay(1000);
-        LED1_OFF();
-        LOS_TaskDelay(1000);
-    }
+	while(1)
+	{
+		LOS_TaskDelay(200);
+		printf("task1 run\r\n");
+	}
 }
-static void Led2Task(void)
+
+void task2(void)
 {
-    while(1)
-    {
-        LED2_ON();
-        LOS_TaskDelay(200);
-        LED2_OFF();
-        LOS_TaskDelay(200);
-    }
+	while(1)
+	{
+		LOS_TaskDelay(100);
+		printf("task2 run\r\n");
+	}
 }
-UINT32 RX_Task_Handle;
-UINT32 TX_Task_Handle;
-static UINT32 AppTaskCreate(void)
+
+uint32_t taskCreate(void)
 {
-    UINT32 uwRet = LOS_OK;
-
-    TSK_INIT_PARAM_S task_init_param;
-
-    task_init_param.usTaskPrio = 4;
-    task_init_param.pcName = "RxTask";
-    task_init_param.pfnTaskEntry = (TSK_ENTRY_FUNC)Led1Task;
-    task_init_param.uwStackSize = 512;
-    uwRet = LOS_TaskCreate(&RX_Task_Handle, &task_init_param);
-    if (uwRet != LOS_OK)
-    {
-        printf("Led1Task create failed,%X\n", uwRet);
-        return uwRet;
-    }
-
-    task_init_param.usTaskPrio = 4;
-    task_init_param.pcName = "TxTask";
-    task_init_param.pfnTaskEntry = (TSK_ENTRY_FUNC)Led2Task;
-    task_init_param.uwStackSize = 512;
-    uwRet = LOS_TaskCreate(&TX_Task_Handle, &task_init_param);
-    if (uwRet != LOS_OK)
-    {
-        printf("Led2Task create failed,%X\n", uwRet);
-        return uwRet;
-    }
-    return LOS_OK;
+	uint32_t retVal;
+	uint32_t task1ID,task2ID;
+	TSK_INIT_PARAM_S taskInitParam;
+	
+	taskInitParam.pcName ="task1";
+	taskInitParam.pfnTaskEntry = (TSK_ENTRY_FUNC)&task1;
+	taskInitParam.usTaskPrio = 4;
+	taskInitParam.uwStackSize = 512;
+	retVal = LOS_TaskCreate(&task1ID,&taskInitParam);
+	if(retVal != LOS_OK)
+	{
+		printf("LOS_TaskCreate() fail,%X\n", retVal);
+	}
+	
+	taskInitParam.pcName ="task2";
+	taskInitParam.pfnTaskEntry = (TSK_ENTRY_FUNC)&task2;
+	taskInitParam.usTaskPrio = 5;
+	taskInitParam.uwStackSize = 512;
+	retVal = LOS_TaskCreate(&task2ID,&taskInitParam);
+	if(retVal != LOS_OK)
+	{
+		printf("LOS_TaskCreate() fail,%X\n", retVal);
+	}
+	return LOS_OK;
 }
-void initGPIO(void)
-{
-    GPIO_InitTypeDef initDef;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
-    initDef.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
-    initDef.GPIO_Mode = GPIO_Mode_Out_PP;
-    initDef.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_Init(GPIOB, &initDef);
-}
-int main()
+int32_t main(void)
 {
-    UINT32 uwRet = LOS_OK;
-		initGPIO();
-    LOS_KernelInit();
-    uwRet = AppTaskCreate();
-    if(uwRet != LOS_OK)
-    {
-        printf("LOS Creat task failed\r\n");
-    }
-    LOS_Start();
+	uint32_t retVal;
+	retVal = LOS_KernelInit();
+	if(retVal != LOS_OK)
+	{
+		printf("LOS_KernelInit() fail,%X\n", retVal);
+	}
+	retVal = taskCreate();
+	if(retVal != LOS_OK)
+	{
+		printf("taskCreate() fail,%X\n", retVal);
+	}
+	LOS_Start();
 }
