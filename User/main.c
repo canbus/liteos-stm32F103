7115a0761ee8795ce20a19dc69d6f6e6853bdf72
canbus <canbus@sohu.com>
2020-08-24 16:55:01 +0800

    定时器基本应用实验 创建两种类型定时器，一个是一次性，一个是周期性

--- User/main.c
+++ User/main.c
@@ -3,6 +3,7 @@
 #include "los_task.ph"
 #include "los_memory.ph"
 #include "los_queue.h"
+#include "los_swtmr.h"
 
 uint32_t taskSendID,taskRevID;
 uint32_t queueID;
@@ -13,22 +14,36 @@ struct DATA{
 };
 
 struct DATA data1={100,"task1 data"};
-struct DATA data2={200,"task2 datas"};
-struct DATA data3={300,"task3 "};
+
+uint16_t timer1ID,timer2ID;
+void time1Handle(uint32_t arg)
+{
+	printf("time over %d\n",arg);
+}
 
 void taskSend(uint32_t arg)
 {
-	uint32_t i,pri;
+	uint32_t i,pri,retVal;
+	//一次性定时器
+	retVal = LOS_SwtmrCreate(100, LOS_SWTMR_MODE_ONCE, time1Handle, &timer1ID, 10 ,OS_SWTMR_ROUSES_IGNORE, OS_SWTMR_ALIGN_INSENSITIVE);
+	if(retVal != LOS_OK)
+	{
+		printf("create timer fail\n");
+	}
+	LOS_SwtmrStart(timer1ID);
+	//重复定时器
+	retVal = LOS_SwtmrCreate(500, LOS_SWTMR_MODE_PERIOD, time1Handle, &timer2ID, 20 ,OS_SWTMR_ROUSES_IGNORE, OS_SWTMR_ALIGN_INSENSITIVE);
+	if(retVal != LOS_OK)
+	{
+		printf("create timer fail\n");
+	}
+	LOS_SwtmrStart(timer2ID);
 	while(1)
 	{
 		LOS_TaskDelay(200);
-		if(arg == data1.id)
-			LOS_QueueWriteCopy(queueID, &data1, sizeof(struct DATA), 0);
-		if(arg == data2.id)
-			LOS_QueueWriteCopy(queueID, &data2, sizeof(struct DATA), 0);
-		if(arg == data3.id)
-			LOS_QueueWriteCopy(queueID, &data3, sizeof(struct DATA), 0);
-		printf("write queue %d\n", arg);
+//		if(arg == data1.id)
+//			LOS_QueueWriteCopy(queueID, &data1, sizeof(struct DATA), 0);
+//		printf("write queue %d\n", arg);
 	}
 }
 
@@ -38,10 +53,10 @@ void taskRev(void)
 	struct DATA queuedata;
 	while(1)
 	{
-		retVal = LOS_QueueReadCopy(queueID, &queuedata, &size, 0);
-		if(retVal == LOS_OK){
-			printf("receive:%d,%s\n", queuedata.id, queuedata.msg);
-		}
+//		retVal = LOS_QueueReadCopy(queueID, &queuedata, &size, 0);
+//		if(retVal == LOS_OK){
+//			printf("receive:%d,%s\n", queuedata.id, queuedata.msg);
+//		}
 		LOS_TaskDelay(10);
 	}
 }
@@ -58,24 +73,6 @@ uint32_t taskCreate(void)
 	taskInitParam.usTaskPrio = 4;
 	taskInitParam.uwStackSize = 512;
 	taskInitParam.uwArg = 100;
-	retVal = LOS_TaskCreate(&taskSendID,&taskInitParam);
-	if(retVal != LOS_OK)
-	{
-		printf("LOS_TaskCreate() fail,%X\n", retVal);
-	}
-	
-	taskInitParam.pcName ="task2";
-	taskInitParam.usTaskPrio = 5;
-	taskInitParam.uwArg = 200;
-	retVal = LOS_TaskCreate(&taskSendID,&taskInitParam);
-	if(retVal != LOS_OK)
-	{
-		printf("LOS_TaskCreate() fail,%X\n", retVal);
-	}
-	
-	taskInitParam.pcName ="task3";
-	taskInitParam.usTaskPrio = 6;
-	taskInitParam.uwArg = 300;
 	retVal = LOS_TaskCreate(&taskSendID,&taskInitParam);
 	if(retVal != LOS_OK)
 	{
